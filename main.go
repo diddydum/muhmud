dc2fabbc63ae0292fad7ad86042152b089dc6ae7
diddydum <37198933+diddydum@users.noreply.github.com>
2018-03-12 17:27:51 -0700

    Be permissive about origin if running locally

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -22,6 +25,7 @@ func setupRouter() *gin.Engine {
 		upgrader := websocket.Upgrader{
 			ReadBufferSize:  1024,
 			WriteBufferSize: 1024,
+			CheckOrigin:     checkOrigin,
 		}
 
 		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
@@ -50,4 +54,23 @@ func main() {
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
+}
+
+func checkOrigin(r *http.Request) bool {
+	origin := r.Header["Origin"]
+	if len(origin) == 0 {
+		return true
+	}
+	u, err := url.Parse(origin[0])
+	if err != nil {
+		return false
+	}
+	// be permissive if running locally
+	reqHost := strings.Split(r.Host, ":")[0]
+	if reqHost == "localhost" && strings.Split(u.Host, ":")[0] == "localhost" {
+		return true
+	}
+
+	// TODO: need to perform case folding?
+	return u.Host == r.Host
 }
